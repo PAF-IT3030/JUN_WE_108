36c52028b8a13d6bb48570379e0d4288107839df
abc <it21349638@my.sliit.lk>
2024-05-06 20:43:17 +0530

    new route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
         <Route path="/api/workout-plans" element={<WorkoutPlansPage />} />
         <Route path="/workout-plan-list" element={<WorkoutPlanList />} />
         <Route path="/all-workout-plans-" element={<AllWorkoutPlans />} />
+        <Route path="/all-workout-plans-" element={<AllWorkoutPlans />} />
       </Routes>
     </Router>
   );
